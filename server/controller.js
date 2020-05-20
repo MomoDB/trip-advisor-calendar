81e9965b5638f3909a5faf7ea59ad2968bccb192
dmondo <dannysmondo@gmail.com>
2020-05-19 18:06:48 -0700

    feat(controller): controller communicates REST API to CRUD db

--- server/controller.js
+++ server/controller.js
@@ -29,4 +29,64 @@ module.exports = {
       }
     });
   }),
+
+  postTrip: () => new Promise((resolve, reject) => {
+    const trip = req.body.data;
+    const today = formatDate();
+    model.postTrip(today, trip, (err, results) => {
+      if (err) {
+        resolve(res.status(500).end());
+      } else {
+        resolve(res.status(200).end());
+      }
+    });
+  }),
+
+  getTrips: () => new Promise((resolve, reject) => {
+    const today = formatDate();
+    model.fetchTrips(today, (err, results) => {
+      if (err) {
+        resolve(res.status(500).end());
+      } else {
+        resolve(res.json(results));;
+      }
+    });
+  }),
+
+  getTrip: () => new Promise((resolve, reject) => {
+    const tripId = req.params.id;
+    const today = formatDate();
+    model.fetchCurrentTrip(today, tripId, (err, results) => {
+      if (err) {
+        resolve(res.status(500).end());
+      } else {
+        resolve(res.json(results));;
+      }
+    });
+  }),
+
+  updateTrip: () => new Promise((resolve, reject) => {
+    const tripId = req.params.id;
+    const trip = req.body.data;
+    const today = formatDate();
+    model.updateTrip(today, trip, tripID, (err, results) => {
+      if (err) {
+        resolve(res.status(500).end());
+      } else {
+        resolve(res.status(200).end());
+      }
+    });
+  }),
+
+  deleteTrip: () => new Promise((resolve, reject) => {
+    const tripId = req.params.id;
+    const today = formatDate();
+    model.deleteTrip(today, tripId, (err, results) => {
+      if (err) {
+        resolve(res.status(500).end());
+      } else {
+        resolve(res.status(200).end());
+      }
+    });
+  }),
 };

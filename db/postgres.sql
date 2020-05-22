abe00e0229b83324dcd2a39edc4b5e2ed294406c
dmondo <dannysmondo@gmail.com>
2020-05-22 16:54:54 -0700

    fix(schema) db schema edits

--- db/postgres.sql
+++ db/postgres.sql
@@ -1,24 +1,38 @@
 DROP DATABASE IF EXISTS calendar;
 CREATE DATABASE calendar;
 
-\c calendar;
-
---each trip has mutiple prices.
---records in price table represent day-packages, date+price
---one to many relationship between trips and prices
+--each trip has mutiple packages.
+--one to many relationship between trips and packages
 CREATE TABLE trips (
   id int not null auto_increment,
+  trip_name varchar(128);
   available boolean not null,
-  detail varchar(1024),
-  duration int(4),
+  overview varchar(256),
+  mobile_ticket boolean,
+  may_cancel boolean,
+  instant_confirm boolean,
+  languages varchar(128),
+  recommend int(3),
+  departure_location varchar(128) not null,
+  return_location varchar(128),
+  includes varchar(128),
+  excludes varchar(128),
+  duration decimal(4,2),
+  info varchar(512),
   PRIMARY KEY(id)
 );
 
-CREATE TABLE prices (
+CREATE TABLE packages (
   id int not null auto_increment,
   trip_id int not null,
   trip_date date not null,
-  price int(4) not null,
+  package_desc varchar(128),
+  duration decimal(4,2) not null,
+  recommend int(3),
+  cancellation date,
+  price_adult int(4) not null,
+  price_youth int(4) not null,
+  price_child int(4) not null,
   PRIMARY KEY(id),
   FOREIGN KEY(trip_id) REFERENCES trips(id)
 );

af9a4907182cafe1cc28c7cc79828610c55af682
dmondo <dannysmondo@gmail.com>
2020-05-22 21:24:21 -0700

    fix(db): update build process

--- db/build-cassandra.sh
+++ db/build-cassandra.sh
@@ -1,9 +1,9 @@
 db/load-csv.sh
 
-cqlsh > schema.cql
+cqlsh -f schema.cql
 
 for i in {0..9}
 do
-  cqlsh > COPY trips FROM 'db/trip'${i}'.csv' WITH DELIMITER="," AND HEADER=TRUE
-  cqlsh > COPY packages FROM 'db/package'${i}'.csv' WITH DELIMITER="," AND HEADER=TRUE
+  cqlsh -f COPY trips FROM 'db/trip'${i}'.csv' WITH DELIMITER="," AND HEADER=TRUE
+  cqlsh -f COPY packages FROM 'db/package'${i}'.csv' WITH DELIMITER="," AND HEADER=TRUE
 done
